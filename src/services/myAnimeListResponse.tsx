60caf6ddc8727e84e4649ddea990d98b1c319337
DarlanHenrique <darlan.silva@ice.ufjf.br>
2025-01-15 17:27:52 -0300

    make changes in response

--- src/services/myAnimeListResponse.tsx
+++ src/services/myAnimeListResponse.tsx
@@ -4,23 +4,26 @@ const CLIENT_ID = 'e59d4d433da1eb9ba473355ee48b91f9';
 export interface Anime {
   id: number;
   name: string;
-  url_image: string;
 }
 
 export interface MyAnimeListResponse {
   results: Anime[];
 }
 
-export const fetchAnimeList = async (id: number = 1015, query: string = 'ranking_type', limit: number = 4): Promise<MyAnimeListResponse> => {
+export const fetchAnimeList = async (query: string = 'one', limit: number = 4): Promise<MyAnimeListResponse> => {
 // https://api.myanimelist.net/v2/anime/ranking?ranking_type=all&limit=4
 // https://api.myanimelist.net/v2/anime?q=one&limit=4
 // https://api.myanimelist.net/v2/anime/30230?fields=id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics
+// https://api.myanimelist.net/v2/anime/season/2017/summer?limit=4
 
 
+  const method = `?q=${query}&limit=${limit}`
   try {
-    // const response = await fetch(`${BASE_URL}?q=${query}&limit=${limit}`, {
-    // const response = await fetch(`${BASE_URL}/ranking?${query}=all&&limit=${limit}`, {
-    const response = await fetch(`${BASE_URL}/ranking?${query}=all&&limit=${limit}`, {
+
+    //ANIME LIST COM NOME: ?q=${query}&limit=${limit}`, {
+    //RANKING GERAL: /ranking?${query}=all&&limit=${limit}`, {
+    //RANKING GERAL: /season/{year}/{season}?limit=4`, {
+    const response = await fetch(`${BASE_URL}${method}`, {
       headers: {
         'X-MAL-CLIENT-ID': CLIENT_ID,
       },
@@ -37,7 +40,7 @@ export const fetchAnimeList = async (id: number = 1015, query: string = 'ranking
       results: data.data.map((anime: any) => ({
         id: anime.node.id,
         name: anime.node.title,
-        url_image: anime.node.main_picture.medium,
+        url_image: anime.node.main_picture.large,
       })),
     };
 
